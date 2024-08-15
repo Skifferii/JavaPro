71819f27c17fa6a863c2f76eb18a702e4744ef5e
Skiferii <dolzhenkoandriy@gmail.com>
2024-08-15 19:28:48 +0200

    Shop

---
+++ lessons/Shop/src/test/java/ait/group44/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package ait.group44.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
